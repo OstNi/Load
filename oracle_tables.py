f3dac1f3c9ef4a14abb6ca0c4d85118153604ea6
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-03-13 15:48:59 +0500

    +venv

--- oracle_tables.py
+++ oracle_tables.py
@@ -1,21 +1,8 @@
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
-    Column, DateTime, ForeignKey, Numeric
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class TPD_CHAPTRES(Base):
-    __tablename__ = 'TPD_CHAPTERS'
-    TC_ID = Column(Integer, primary_key=True)
-    SORT = Column(Integer, nullable=False)
-    TC_COMMENT = Column(String(4_000), nullable=True)
-    EXAM = Column(String(1), nullable=True)
-    NAME = Column(String(240), nullable=False)
-    TC_TC_ID = Column(Integer, nullable=True)
-    TP_TP_ID = Column(Integer, nullable=False)
-    TCTP_TCTP_ID = Column(Integer, nullable=True)
-    TPDL_TPDL_ID = Column(Integer, nullable=True)
-
+# описание дата классов таблиц oracle
+from dataclasses import dataclass
+@dataclass
+class ses:
+    line_2: str
 
 
 
--- orcl_education_db.py
+++ orcl_education_db.py
@@ -1,8 +1,6 @@
 from setting import connect_setting_oracle, path
-from sqlalchemy import create_engine, Table, Column, Integer, MetaData, select, String, text
+from sqlalchemy import create_engine, text
 import cx_Oracle
-import numpy as np
-import pandas as pd
 from dataclasses import dataclass
 
 
@@ -19,31 +17,19 @@ ENGINE_PATH: str = (
         connect_setting_oracle['SERVICE']
 )
 
-@dataclass
-class ses:
-    line_2: str
-    # l3: int
-    # l4: int
-    # l5: int
-    # l6: str
-    # l7: int
-    # l8: str
-
-
-dict_1 = dict()
 
 engine = create_engine(ENGINE_PATH)
-with engine.connect() as conn:
-    result = conn.execute(text('SELECT TOS_ID, TYPE_INFO  FROM TYPE_OF_SES'))
-    for r in result:
-        dict_1[r[0]] = ses(r[1])
-    # data = result.fetchall()
-    # print(result.keys())
-    # for i in data:
-    #     print(i)
+# with engine.connect() as conn:
+#     result = conn.execute(text('SELECT TOS_ID, TYPE_INFO  FROM TYPE_OF_SES'))
+#     for r in result:
+#         pass
 
-print(dict_1)
-# df = pd.read_sql_table('TYPE_OF_SES', con=engine)
-# print(df)
+
+
+
+
+
+
+
 
 
