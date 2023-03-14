c3be3791037dc9a4d31d98b3e9864fe0dc503cba
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-03-14 17:44:24 +0500

    +venv

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from oracle_tables import NrGroup, DisStudy
 from orcl_education_db import get_table
 from time import time
 
@@ -7,16 +6,16 @@ def main():
     start = time()
     sql = 'SELECT NG_ID, NAME, FULLNAME, SUBNAME, ST_CNT, FFS_FFS_ID, ' \
           'NG_NG_ID, TOG_TOG_ID, TY_TY_ID, FFD_FFD_ID FROM NR_GROUPS'
-    nr_group = get_table(sql, NrGroup)
+    nr_group = get_table('NR_GROUPS')
     end = time()
     print(f'nr_group time: {round((end - start),2)} sec | count: {len(nr_group)}')
 
-    start = time()
-    sql = 'SELECT DSS_ID, DIS_DIS_ID, TEDP_TEDP_ID, DIV_DIV_ID, FOE_FOE_ID, NUM, ' \
-          'STU_YEAR, BCH_BCH_ID, FCR_FCR_ID, TPDL_TPDL_ID FROM DIS_STUDIES'
-    dis_study = get_table(sql, DisStudy)
-    end = time()
-    print(f'dis_study time: {round((end - start), 2)} sec | count: {len(dis_study)}')
+    # start = time()
+    # sql = 'SELECT DSS_ID, DIS_DIS_ID, TEDP_TEDP_ID, DIV_DIV_ID, FOE_FOE_ID, NUM, ' \
+    #       'STU_YEAR, BCH_BCH_ID, FCR_FCR_ID, TPDL_TPDL_ID FROM DIS_STUDIES'
+    # dis_study = get_table(sql, DisStudy)
+    # end = time()
+    # print(f'dis_study time: {round((end - start), 2)} sec | count: {len(dis_study)}')
 
 
 if __name__ == '__main__':
--- orcl_education_db.py
+++ orcl_education_db.py
@@ -1,6 +1,9 @@
 from setting import connect_setting_oracle, path
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, inspect, Engine, MetaData, Table
+from dataclasses import make_dataclass
 import cx_Oracle
+import re
+
 cx_Oracle.init_oracle_client(lib_dir=path['LIB_DIR'], config_dir=path['CONFIG_DIR'])
 
 ENGINE_PATH: str = (
@@ -14,17 +17,74 @@ ENGINE_PATH: str = (
 )
 
 
-def get_table(sql: str, dataclass) -> dict:
-    '''
-    Получаем словарь из таблицы oracle, в которой key=id и значение это dataclass
-    sql - запрос для получения таблицы
-    dataclass - датакласс для стрк
-    '''
-    engine = create_engine(ENGINE_PATH)
-    nr_group = dict()
-    with engine.connect() as conn:
-        groups = conn.execute(text(sql))
-        for group in groups:
-            nr_group[group[0]] = dataclass(*[i for i in group[1::1]])
+def get_table(table_name) -> dict:
+    out_table = dict()
+    engine = create_engine(ENGINE_PATH, echo=True)
+    attr = _get_attr(table_name, engine)
+    pk_idx = _get_pk_idx(attr, engine, table_name)
+    attr = re_attr(pk_idx, attr)
+    meta = get_meta(table_name, attr)
 
-    return nr_group
+    with engine.connect() as conn:
+        table = conn.execute(text(f'SELECT * FROM {table_name}'))
+        for item in table:
+            out_table[tuple([item[i] for i in pk_idx])] = meta(*[item[i] for i in range(len(item)) if i not in pk_idx])
+
+    return out_table
+
+
+def _get_pk(table_name, engine: Engine) -> list:
+    sql = f"SELECT ucc.COLUMN_NAME " \
+          "FROM USER_CONSTRAINTS uc " \
+          ",USER_CONS_COLUMNS ucc " \
+          f"WHERE uc.TABLE_NAME = '{table_name}' " \
+          "AND uc.CONSTRAINT_TYPE  = 'P' " \
+          "AND ucc.CONSTRAINT_NAME = uc.CONSTRAINT_NAME "
+
+    pk = list()
+    with engine.connect() as conn:
+        table = conn.execute(text(sql))
+        for item in table:
+            pk += list(item)
+    pk = [i.lower() for i in pk]
+
+    return pk
+
+
+def _get_attr(table_name, engine: Engine) -> list:
+    attr = list()
+    inspector = inspect(engine)
+    columns = inspector.get_columns(f'{table_name}')
+    for column in columns:
+        attr.append((column['name'], _get_type_attr(str(column['type']))))
+
+    return attr
+
+
+def _get_type_attr(oracle_type):
+    if re.search('NUMBER', oracle_type) or re.search('INTEGER', oracle_type):
+        return int
+    if re.search('VARCHAR', oracle_type):
+        return str
+    return str
+
+
+def get_meta(name, attr):
+    return make_dataclass(str(name), attr)
+
+
+def _get_pk_idx(attr: list[tuple], engine: Engine, table_name) -> list:
+    pk = _get_pk(table_name, engine)
+    pk_idx = list()
+    for idx, key in enumerate(attr):
+        if key[0] in pk:
+            pk_idx.append(idx)
+
+    return pk_idx
+
+
+def re_attr(pk_idx: list, attr: list) -> list:
+    for idx in pk_idx:
+        attr.pop(idx)
+
+    return attr
