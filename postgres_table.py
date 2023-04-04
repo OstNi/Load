213c96427b0cada60730ac6692be75e1db354cac
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-04-04 17:46:33 +0500

    + peewee

--- postgres_table.py
+++ postgres_table.py
@@ -1,14 +1,41 @@
-from sqlalchemy import create_engine
+from peewee import *
+from pwiz import *
 from setting import connect_setting_postgres
 
-engine = create_engine(
-    connect_setting_postgres['DIALECT'] + '://' +
-    connect_setting_postgres['USERNAME'] + ':' +
-    connect_setting_postgres['PASSWORD'] + '@' +
-    connect_setting_postgres['HOST'] + ':' +
-    connect_setting_postgres['PORT'] + '/' +
-    connect_setting_postgres['DATABASE']
+pg_db = PostgresqlDatabase("postgres",
+                           user=connect_setting_postgres['USERNAME'],
+                           password=connect_setting_postgres['PASSWORD'],
+                           host=connect_setting_postgres['HOST'],
+                           port=connect_setting_postgres['PORT']
 )
 
-with engine.connect() as conn:
-    print('Success')
+# Models
+models = generate_models(pg_db, schema="public", table_names=["stu_groups"])
+Stu_groups = models["stu_groups"]
+
+
+def _get_attr(model: str):
+    attr: list = []
+    # итерируемся по атрибутам класса, исключая Protected Attributes и DoesNotExist
+    for item in vars(model):
+        if item[0] != '_' and item != 'DoesNotExist':
+            attr.append(item)
+    return attr
+
+
+class BaseModel(Model):
+    class Meta:
+        database = pg_db
+
+
+class StuGroup(BaseModel):
+    sgr_id = AutoField(column_name="sgr_id")
+    name = CharField(max_length=240)
+    dgr_id = IntegerField()
+    sgr_sgr_id = IntegerField()
+    info = CharField(max_length=1000)
+
+
+
+
+
