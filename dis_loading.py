71f8c2abd437e00eaac7b4a332ac51e10bfbb77e
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-04-20 17:46:32 +0500

    fixed errors + update

--- dis_loading.py
+++ dis_loading.py
@@ -1,12 +1,19 @@
 from postgres_model import *
 from dis_group import *
+from oracle_table import call_oracle_function
 from dataclasses import dataclass
 from datetime import datetime
+from log import _init_logger
+import logging
 
 
 """
 Алгоритм выгрузки со стороны DIS_GROUP
 """
+
+# инициализируем лог
+_init_logger('dis_loading')
+logger = logging.getLogger('dis_load.main')
 
 
 def create_stu_group(dis_group: dataclass, dgr_id: int) -> StuGroups | None:
@@ -18,23 +25,31 @@ def create_stu_group(dis_group: dataclass, dgr_id: int) -> StuGroups | None:
     """
     if not model_contains(model=StuGroups, key="dgr_id", values=dgr_id):
         # Проверка наличия верхнего уровня
+
+        hight_lvl = None
         if dis_group.dgr_dgr_id:
             if not (hight_lvl := StuGroups.get_or_none(dgr_id=dis_group.dgr_dgr_id)):
                 logger.debug(f"Для группы dgr_id {dgr_id} не было созданого "
                              f"верхнего уровня dgr_dgr_id {dis_group.dgr_dgr_id}")
 
+        if hight_lvl:
+            sgr_id = hight_lvl.sgr_id
+        else:
+            sgr_id = None
+
         new_group = StuGroups(
             name=dis_group.fullname,
             dgr_id=dgr_id,
-            sgr_sgr_id=hight_lvl.sgr_id if hight_lvl else None,
-        ).save()
+            sgr_sgr_id=sgr_id,
+        )
 
+        new_group.save()
         return new_group
 
     return None
 
 
-def create_tp_delivery(tpdl_id: int) -> TpDeliveries:
+def create_tp_delivery(tpdl_id: int) -> TpDeliveries | None:
     """
     Создание модели TpDeliveries postgres_model.py
     :param tpdl_id: id схемы доставки
@@ -44,13 +59,18 @@ def create_tp_delivery(tpdl_id: int) -> TpDeliveries:
     tpdl_key = list(oracle_tp_deliveries.keys())[0]
 
     if not model_contains(model=TpDeliveries, key="tpdl_id", values=tpdl_id):
+
+        # Создаем TEACH_PROGRAM
+        teach_program = create_teach_program(oracle_tp_deliveries[tpdl_key].tp_tp_id)
+
         new_delivery = TpDeliveries.create(
             tpdl_id=tpdl_id,
             name=oracle_tp_deliveries[tpdl_key].name,
-            tpr_tpr=oracle_tp_deliveries[tpdl_key].tp_tp_id
+            tpr_tpr=teach_program.tpr_id
         )
+        return new_delivery
 
-    return new_delivery if new_delivery else TpDeliveries.get(tpdl_id=tpdl_id)
+    return TpDeliveries.get(tpdl_id=tpdl_id)
 
 
 def create_tpr_chapters(tpdl_id: int) -> list:
@@ -59,43 +79,60 @@ def create_tpr_chapters(tpdl_id: int) -> list:
     :param tpdl_id: id схемы доставки
     :return: объект класса TprChapters postgres_model.py
     """
-    oracle_tc_chapter = get_tpd_from_tpdl(tpdl_id)
-    tc_id = list(oracle_tc_chapter.keys())[0]
-
+    oracle_tc_chapter = get_tpd_from_tpdl(tpdl_id)  # список всех TC с такой tpd_id
     tpr_chapters = []
 
-    if not model_contains(model=TprChapters, key="tc_id", values=tc_id[0]):
-        tpr_chapters.append(TprChapters(
-            tc_id=tc_id[0],
-            name=oracle_tc_chapter[tc_id].name,
-            ext_ext=oracle_tc_chapter[tc_id].exam,
-            str=oracle_tc_chapter[tc_id].sort,
-            tpr_tpr=oracle_tc_chapter[tc_id].tp_tp_id,
-            tpdl_tpdl=tpdl_id
-        ).save())
+    for tc_id, value in oracle_tc_chapter.items():
+        if not model_contains(model=TprChapters, key="tc_id", values=tc_id[0]):
+            new_chapter = TprChapters(
+                tc_id=tc_id[0],
+                name=value.name,
+                ext_ext=value.exam,
+                srt=value.sort,
+                tpr_tpr=value.tp_tp_id,
+                tpdl_tpdl=tpdl_id
+            )
+            tpr_chapters.append(new_chapter)
+            new_chapter.save()
+        else:
+            tpr_chapters.append(TprChapters.get(tc_id=tc_id[0]))
 
     return tpr_chapters
 
 
-def create_tc_time(tc_id: int, type_of_work_id: int) -> TcTimes:
+def create_tc_time(tc_id: int, type_of_work_id: int, ty_id: int) -> TcTimes:
     """
     Создание модели TcTimes postgres_model.py
     :param tc_id: id учебного раздела
     :param type_of_work_id: id типа работ
+    :param ty_id: id учебного года
     :return: объект класса TcTimes postgres_model.py
     """
     oracle_tc_time = get_tc_time(tc_id, type_of_work_id)
     totc_id = list(oracle_tc_time.keys())[0]
 
+    # аргументы для oracle функции, котоорая сичтает количество точек контрроля
+    agrs_for_func = {
+        "P_TC_ID": tc_id,
+        "P_TOW_ID": type_of_work_id,
+        "P_TY_ID": ty_id
+    }
+
+    count_of_ctl = call_oracle_function("tpd_t.GET_CTL_RPNT_CNT", args=agrs_for_func)
+
     if not model_contains(model=TcTimes, key="totc_id", values=totc_id[0]):
         new_tc_time = TcTimes(
             totc_id=totc_id[0],
-            ctl_count=oracle_tc_time[totc_id].val_check,
-            tch_tch=tc_id[0],
+            ctl_count=count_of_ctl if count_of_ctl else 0,
+            tch_tch=TprChapters.get(tc_id=tc_id).tch_id,
             val=oracle_tc_time[totc_id].value,
             wt_wot=type_of_work_id
-        ).save()
-    return new_tc_time if new_tc_time else TcTimes.get(totc_id=totc_id[0])
+        )
+
+        new_tc_time.save()
+        return new_tc_time
+
+    return TcTimes.get(totc_id=totc_id[0])
 
 
 def create_teach_program(tp_id: int) -> TeachPrograms:
@@ -107,16 +144,19 @@ def create_teach_program(tp_id: int) -> TeachPrograms:
     oracle_teach_program = get_teach_program(tp_id)
     if not model_contains(model=TeachPrograms, key="tpr_id", values=tp_id):
         new_teach_program = TeachPrograms.create(
-            tpd_id=tp_id,
+            tpr_id=tp_id,
             confirm_date=oracle_teach_program[(tp_id,)].date_of_confirm,
             dis_dis=oracle_teach_program[(tp_id,)].dis_dis_id,
             practice_form=oracle_teach_program[(tp_id,)].practice_form,
             practice_schedule=oracle_teach_program[(tp_id,)].practice_schedule,
             protocol=oracle_teach_program[(tp_id,)].commend_protocol,
-            status=oracle_teach_program[(tp_id,)].status
+            status=oracle_teach_program[(tp_id,)].status,
+            tpt_tpt=oracle_teach_program[(tp_id,)].ttp_ttp_id
         )
 
-    return new_teach_program if new_teach_program else TeachPrograms.get(tpr_id=tp_id)
+        return new_teach_program
+
+    return TeachPrograms.get(tpr_id=tp_id)
 
 
 def create_discipline(dis_id: int) -> Disciplines:
@@ -133,7 +173,9 @@ def create_discipline(dis_id: int) -> Disciplines:
             name=oracle_discipline[(dis_id,)].name
         )
 
-    return new_discipline if new_discipline else Disciplines.get(dis_id=dis_id)
+        return new_discipline
+
+    return Disciplines.get(dis_id=dis_id)
 
 
 def create_version() -> Versions:
@@ -142,14 +184,15 @@ def create_version() -> Versions:
     :return: объект класса Version postgres_model.py
     """
     new_version = Versions(
-        calc_date=datetime.now,
+        calc_date=datetime.now(),
         info="dis_group loading"
-    ).save()
+    )
 
+    new_version.save()
     return new_version
 
 
-def create_group_work(sgr_id: int, type_of_work_id: int) -> WorkTypes:
+def create_group_work(sgr_id: int, type_of_work_id: int) -> GroupWorks:
     """
     Создание модели WorkTypes postgres_model.py
     :param sgr_id: id stu_group
@@ -159,8 +202,9 @@ def create_group_work(sgr_id: int, type_of_work_id: int) -> WorkTypes:
     new_group_work = GroupWorks(
         sgr_sgr=sgr_id,
         wt_wot=type_of_work_id
-    ).save()
+    )
 
+    new_group_work.save()
     return new_group_work
 
 
@@ -171,9 +215,12 @@ def create_group_faculty(dis_groups: dataclass, dgr_id: tuple, sgr_id: int) -> N
     :param dgr_id: id текущей группы
     :param sgr_id: id stu_group
     """
-    low_dgr_id = find_low_lvl_group(dis_groups, dgr_id)
-    pl_list = get_pr_list(low_dgr_id[0])
 
+    # Если это не нижний уровень, то не создаем  GROUP_FACULTY
+    if not is_low_dgr_group(dis_groups, dgr_id[0]):
+        return
+
+    pl_list = get_pr_list(dgr_id[0])
     dif_group_fuculty = dict()
     for pr_item in pl_list:
         data = get_group_faculty_info(pr_item)
@@ -187,23 +234,25 @@ def create_group_faculty(dis_groups: dataclass, dgr_id: tuple, sgr_id: int) -> N
 
     for info, value in dif_group_fuculty.items():
         # Создаем GROUP_FACULTY
-        GroupFaculties(
+        new_group_faculty = GroupFaculties(
             efo_efo=info[1],
             stu_count=value,
-            num_course=get_num_of_course(low_dgr_id[0]),
+            num_course=get_num_of_course(dgr_id[0]),
             sgr_sgr=sgr_id,
             div_div=info[0],
             ele_ele=info[2]
-        ).save()
+        )
+        new_group_faculty.save()
 
 
-def is_low_stu_group(sgr_id: int) -> bool:
+def is_low_dgr_group(dis_groups: dict, dgr_id: int) -> bool:
     """
     Определяет: является ли эта группа нижним урровнем
-    :param sgr_id: id stu_group
+    :param dis_groups: все DIS_GROUPS
+    :param dgr_id: id текущей группы
     :return: True - нижний уровень, иначе - False
     """
-    if not any(stu_group.sgr_sgr_id == sgr_id for stu_group in StuGroups.select()):
+    if not any(dis_group.dgr_dgr_id == dgr_id for dis_group in dis_groups.values()):
         return True
     return False
 
@@ -211,7 +260,7 @@ def is_low_stu_group(sgr_id: int) -> bool:
 def find_low_lvl_group(dis_groups: dict, dgr_id: tuple) -> tuple:
     """
     Находи нижний уровень DIS_GROUPS (студенты есть только на нижних уровнях)
-    :param dis_group: все DIS_GROUPS группы
+    :param dis_groups: все DIS_GROUPS группы
     :param dgr_id: id DIS_GROUP
     :return: dgr_id группы нижнего уровня
     """
@@ -229,11 +278,11 @@ def choice_of_branch(study_type: str, dis_study: dataclass, dis_groups: dataclas
     :param dgr_id: id текущей группы
     """
     branches = {
-        "эл": elective_branch(dis_study),
-        "фак": facultative_branch(dis_study),
-        "дпв": dpv_branch(dis_study, dis_groups, dgr_id)
+        "эл": lambda: elective_branch(dis_study),
+        "фак": lambda: facultative_branch(dis_study),
+        "дпв": lambda: dpv_branch(dis_study, dis_groups, dgr_id)
     }
-    return branches.get(study_type)
+    return branches.get(study_type)()
 
 
 def elective_branch(dis_study: dataclass) -> TpDeliveries:
@@ -273,7 +322,7 @@ def dpv_branch(dis_study: dataclass, dis_groups: dataclass, dgr_id: tuple) -> Tp
 
     pr_tpdl_lst = [[i] for i in pr_lst]
     for i in range(len(pr_tpdl_lst)):
-        pr_tpdl_lst[i].append(get_tpdl(pr_lst[i], dis_study.dis_dis_id))
+        pr_tpdl_lst[i].append(get_tpdl_use_pr_dis(pr_lst[i], dis_study.dis_dis_id))
 
     # Сравниваем учебные нагрузки среди студентов группы
     if not checker(dgr_id[0], pr_tpdl_lst):
@@ -287,6 +336,9 @@ def dpv_branch(dis_study: dataclass, dis_groups: dataclass, dgr_id: tuple) -> Tp
 
 def main():
 
+    # Создаем VERSION
+    version = create_version()
+
     # реализация логики выгрузки
     dis_groups = get_dis_groups()   # выгружаем дис группы
     for key in dis_groups:
@@ -295,6 +347,9 @@ def main():
 
         if dis_studies[dds_id].foe_foe_id != 1:     # если это не очная форма обучения, пропускаем
             continue
+
+        # Создаем Disciplines
+        create_discipline(dis_studies[dds_id].dis_dis_id)
 
         # Получаем и создаем TpDelivery исходя из типа дисциплины
         # проверка нужна на случай, если учебная нагрузка среди студентов различна,
@@ -316,15 +371,6 @@ def main():
         # Создаем TC
         tpr_chapters = create_tpr_chapters(tp_delivery.tpdl_id)
 
-        # Создаем TEACH_PROGRAM
-        teach_program = create_teach_program(tp_delivery.tpr_tpr)
-
-        # Создаем DISCIPLINE
-        discipline = create_discipline(teach_program.dis_dis)
-
-        # Создаем VERSION
-        version = create_version()
-
         # Создаем DGR_PERIOD на каждый ty_period, в котором обучается группа
         for typ_id, tpr_chapter in zip(get_ty_period_range(key[0]), tpr_chapters):
 
@@ -333,15 +379,15 @@ def main():
                 sgr_sgr=stu_group.sgr_id,
                 tch_tch=tpr_chapter.tch_id,
                 ver_ver=version.ver_id,
-                #div_div=
+                div_div=2619,
                 typ_typ=typ_id
             ).save()
 
-            # Создаем TC_TIME
-            tc_time = create_tc_time(tpr_chapter.tc_id, work_type)
-
             # Вызываем TyPeriod, чтобы узнать TeachYears (ty_id)
             ty_period = TyPeriods.get(typ_id=typ_id)
+
+            # Создаем TC_TIME
+            tc_time = create_tc_time(tpr_chapter.tc_id, work_type, ty_period.ty_ty.ty_id)
 
         # Создаем GROUP_WORKS
         group_work = create_group_work(stu_group.sgr_id, work_type)
@@ -349,6 +395,9 @@ def main():
         # Создаем GROUP_FACULTY
         create_group_faculty(dis_groups, key, stu_group.sgr_id)
 
+
+if __name__ == "__main__":
+    main()
 
 
 
