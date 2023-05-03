57e2a6dab14771447bb3551ba1c29809005216d6
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-05-03 18:38:09 +0500

    + exceptions

---
+++ exceptions.py
@@ -0,0 +1,34 @@
+class StuGroupExc(Exception):
+    pass
+
+
+class TpDeliveryExc(Exception):
+    pass
+
+
+class TprChapterExc(Exception):
+    pass
+
+
+class DgrPeriodExc(Exception):
+    pass
+
+
+class TeachProgramExc(Exception):
+    pass
+
+
+class DisciplineExc(Exception):
+    pass
+
+
+class TcTimeExc(Exception):
+    pass
+
+
+class GroupWorkExc(Exception):
+    pass
+
+
+class GroupFacultyExc(Exception):
+    pass
