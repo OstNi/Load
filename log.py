9d0a808a3f724a235633680b4d750af1f140ee5c
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-05-04 19:59:41 +0500

    + update setting

--- log.py
+++ log.py
@@ -10,18 +10,18 @@ def _init_logger(name, filename: str | None = None):
     directory_path = "logs/"
     logger = logging.getLogger(name)
     logger.propagate = False
-    format = '%(asctime)s :: %(name)s:%(lineno)s :: %(levelname)s :: %(message)s'
+    fmt = '%(asctime)s :: %(name)s:%(lineno)s :: %(levelname)s :: %(message)s'
     logger.setLevel(logging.DEBUG)
     sh = logging.StreamHandler()
-    sh.setFormatter(logging.Formatter(format))
+    sh.setFormatter(logging.Formatter(fmt))
     sh.setLevel(logging.DEBUG)
     fh = logging.handlers.RotatingFileHandler(
         filename='logs/test.log' if not filename else directory_path + filename,
         maxBytes=1250000,
         backupCount=1
     )
-    fh.setFormatter(logging.Formatter(format))
-    fh.setLevel(logging.ERROR)
+    fh.setFormatter(logging.Formatter(fmt))
+    fh.setLevel(logging.INFO)
     logger.addHandler(sh)
     logger.addHandler(fh)
 
